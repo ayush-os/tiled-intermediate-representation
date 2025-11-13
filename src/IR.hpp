8c75d99a192c61c54b0d6ddb9314f2f2d835e390
ayush <99374392+ayush-os@users.noreply.github.com>
2025-11-13 10:20:17 -0800

    updated IR

--- src/IR.hpp
+++ src/IR.hpp
@@ -3,187 +3,173 @@
 #include <cstddef>
 #include <vector>
 
-using ConstValue = std::variant<
-    int,       // Int32
-    long long, // Int64
-    float,     // Float32
-    double     // Float64
-    >;
+using ConstValue = std::variant<int,       // Int32
+                                long long, // Int64
+                                float,     // Float32
+                                double     // Float64
+                                >;
 
-enum class DType
-{
-    Float32,
-    Float64,
-    Int32,
-    Int64,
+enum class DType {
+  Float32,
+  Float64,
+  Int32,
+  Int64,
 };
 
-enum class IRNodeType
-{
-    // Structural Nodes
-    Loop,
-    // Access Nodes
-    Load,
-    Store,
-    // Computation Nodes
-    Add,
-    Mul,
-    Assign,
-    // Control Flow / Expression Nodes
-    Const,     // For constants like 0, N, T, etc.
-    Variable,  // For loop indices i, j, ii, jj
-    Min,       // Ex MIN(ii + T, N) in tiling bounds
-    ScalerRef, // L-type for Assign Nodes
+enum class IRNodeType {
+  // Structural Nodes
+  Loop,
+  // Access Nodes
+  Load,
+  Store,
+  // Computation Nodes
+  Add,
+  Mul,
+  Assign,
+  // Control Flow / Expression Nodes
+  Const,    // For constants like 0, N, T, etc.
+  Variable, // For loop indices i, j, ii, jj
+  Min,      // Ex MIN(ii + T, N) in tiling bounds
 };
 
-class Tensor
-{
+class IRNode {
 public:
-private:
-    DType dtype_;
-    size_t dims_;
-    std::vector<size_t> extents_;
-    std::vector<size_t> strides_;
+  virtual ~IRNode() = default;
+  virtual IRNodeType getType() const = 0;
 };
 
-class IRNode
-{
+class Tensor {
 public:
-    virtual ~IRNode() = default;
+  Tensor(std::string n, DType d, size_t dims,
+         const std::vector<size_t> &extents)
+      : name(std::move(n)), dtype_(d), dims_(dims), extents_(extents) {
+    if (dims != extents.size()) {
+      throw std::invalid_argument("dims must match the size of extents");
+    }
 
-    virtual IRNodeType getType() const = 0;
+    strides_.resize(dims_);
+
+    size_t current_stride = 1;
+
+    for (int i = dims_ - 1; i >= 0; --i) {
+      strides_[i] = current_stride;
+      // The stride for the next (previous index) dimension
+      // is the current stride multiplied by the extent of the current
+      // dimension.
+      current_stride *= extents_[i];
+    }
+  }
+
+  std::string name;
+  DType dtype_;
+  size_t dims_;
+  std::vector<size_t> extents_;
+  std::vector<size_t> strides_;
 };
 
-class Load : public IRNode
-{
+class Const : public IRNode {
 public:
-    Load(Tensor &t, std::vector<std::unique_ptr<IRNode>> indices)
-        : tensor_(t), indices_(std::move(indices)) {}
+  Const(ConstValue val, DType type) : value_(std::move(val)), dtype_(type) {}
 
-    IRNodeType getType() const override { return IRNodeType::Load; }
+  IRNodeType getType() const override { return IRNodeType::Const; }
 
-private:
-    Tensor &tensor_;
-    std::vector<std::unique_ptr<IRNode>> indices_;
+  const ConstValue &getValue() const { return value_; }
+  DType getDType() const { return dtype_; }
+
+  ConstValue value_;
+
+  DType dtype_;
 };
 
-class Store : public IRNode
-{
+class Variable : public IRNode {
 public:
-    Store(Tensor &t, std::vector<std::unique_ptr<IRNode>> indices)
-        : tensor_(t), indices_(std::move(indices)) {}
+  Variable(std::string name) : name_(std::move(name)) {}
 
-    IRNodeType getType() const override { return IRNodeType::Store; }
+  IRNodeType getType() const override { return IRNodeType::Variable; }
 
-private:
-    Tensor &tensor_;
-    std::vector<std::unique_ptr<IRNode>> indices_;
+  const std::string &getName() const { return name_; }
+
+  std::string name_;
 };
 
-class Loop : public IRNode
-{
+class Min : public IRNode {
 public:
-    Loop(std::string i, std::unique_ptr<IRNode> lb, std::unique_ptr<IRNode> ub, std::unique_ptr<IRNode> step)
-        : index_(std::move(i)), lower_bound_(std::move(lb)), upper_bound_(std::move(ub)), step_(std::move(step)) {}
+  Min(std::unique_ptr<IRNode> one, std::unique_ptr<IRNode> two)
+      : operand_one_(std::move(one)), operand_two_(std::move(two)) {}
 
-    IRNodeType getType() const override { return IRNodeType::Loop; }
+  IRNodeType getType() const override { return IRNodeType::Min; }
 
-    std::vector<std::unique_ptr<IRNode>> body_;
-
-private:
-    std::string index_;
-    std::unique_ptr<IRNode> lower_bound_;
-    std::unique_ptr<IRNode> upper_bound_;
-    std::unique_ptr<IRNode> step_;
+  std::unique_ptr<IRNode> operand_one_;
+  std::unique_ptr<IRNode> operand_two_;
 };
 
-class Add : public IRNode
-{
+class Add : public IRNode {
 public:
-    Add(std::unique_ptr<IRNode> one, std::unique_ptr<IRNode> two) : operand_one_(std::move(one)), operand_two_(std::move(two)) {}
+  Add(std::unique_ptr<IRNode> one, std::unique_ptr<IRNode> two)
+      : operand_one_(std::move(one)), operand_two_(std::move(two)) {}
 
-    IRNodeType getType() const override { return IRNodeType::Add; }
+  IRNodeType getType() const override { return IRNodeType::Add; }
 
-private:
-    std::unique_ptr<IRNode> operand_one_;
-    std::unique_ptr<IRNode> operand_two_;
+  std::unique_ptr<IRNode> operand_one_;
+  std::unique_ptr<IRNode> operand_two_;
 };
 
-class Mul : public IRNode
-{
+class Mul : public IRNode {
 public:
-    Mul(std::unique_ptr<IRNode> one, std::unique_ptr<IRNode> two) : operand_one_(std::move(one)), operand_two_(std::move(two)) {}
+  Mul(std::unique_ptr<IRNode> one, std::unique_ptr<IRNode> two)
+      : operand_one_(std::move(one)), operand_two_(std::move(two)) {}
 
-    IRNodeType getType() const override { return IRNodeType::Mul; }
+  IRNodeType getType() const override { return IRNodeType::Mul; }
 
-private:
-    std::unique_ptr<IRNode> operand_one_;
-    std::unique_ptr<IRNode> operand_two_;
+  std::unique_ptr<IRNode> operand_one_;
+  std::unique_ptr<IRNode> operand_two_;
 };
 
-class Assign : public IRNode
-{
+class Load : public IRNode {
 public:
-    Assign(std::unique_ptr<IRNode> target, std::unique_ptr<IRNode> value)
-        : target_(std::move(target)), value_(std::move(value)) {}
+  Load(Tensor &t, std::vector<std::unique_ptr<IRNode>> indices)
+      : tensor_(t), indices_(std::move(indices)) {}
 
-    IRNodeType getType() const override { return IRNodeType::Assign; }
+  IRNodeType getType() const override { return IRNodeType::Load; }
 
-private:
-    std::unique_ptr<IRNode> target_;
-    std::unique_ptr<IRNode> value_;
+  Tensor &tensor_;
+  std::vector<std::unique_ptr<IRNode>> indices_;
 };
 
-class Const : public IRNode
-{
+class Store : public IRNode {
 public:
-    Const(ConstValue val, DType type) : value_(std::move(val)), dtype_(type) {}
+  Store(Tensor &t, std::vector<std::unique_ptr<IRNode>> indices)
+      : tensor_(t), indices_(std::move(indices)) {}
 
-    IRNodeType getType() const override { return IRNodeType::Const; }
+  IRNodeType getType() const override { return IRNodeType::Store; }
 
-    const ConstValue &getValue() const { return value_; }
-    DType getDType() const { return dtype_; }
-
-private:
-    ConstValue value_;
-
-    DType dtype_;
+  Tensor &tensor_;
+  std::vector<std::unique_ptr<IRNode>> indices_;
 };
 
-class Variable : public IRNode
-{
+class Loop : public IRNode {
 public:
-    Variable(std::string name) : name_(std::move(name)) {}
+  Loop(std::string i, std::unique_ptr<IRNode> lb, std::unique_ptr<IRNode> ub,
+       std::unique_ptr<IRNode> step)
+      : index_(std::move(i)), lower_bound_(std::move(lb)),
+        upper_bound_(std::move(ub)), step_(std::move(step)) {}
 
-    IRNodeType getType() const override { return IRNodeType::Variable; }
+  IRNodeType getType() const override { return IRNodeType::Loop; }
 
-    const std::string &getName() const { return name_; }
-
-private:
-    std::string name_;
+  std::string index_;
+  std::unique_ptr<IRNode> lower_bound_;
+  std::unique_ptr<IRNode> upper_bound_;
+  std::unique_ptr<IRNode> step_;
+  std::vector<std::unique_ptr<IRNode>> body_;
 };
 
-class Min : public IRNode
-{
+class Assign : public IRNode {
 public:
-    Min(std::unique_ptr<IRNode> one, std::unique_ptr<IRNode> two) : operand_one_(std::move(one)), operand_two_(std::move(two)) {}
+  Assign(std::unique_ptr<IRNode> target, std::unique_ptr<IRNode> value)
+      : target_(std::move(target)), value_(std::move(value)) {}
 
-    IRNodeType getType() const override { return IRNodeType::Min; }
+  IRNodeType getType() const override { return IRNodeType::Assign; }
 
-private:
-    std::unique_ptr<IRNode> operand_one_;
-    std::unique_ptr<IRNode> operand_two_;
-};
-
-class ScalarRef : public IRNode
-{
-public:
-    ScalarRef(std::string name) : name_(std::move(name)) {}
-
-    IRNodeType getType() const override { return IRNodeType::ScalerRef; }
-
-    const std::string &getName() const { return name_; }
-
-private:
-    std::string name_;
+  std::unique_ptr<IRNode> target_;
+  std::unique_ptr<IRNode> value_;
 };
